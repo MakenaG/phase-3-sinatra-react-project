e46da94883fd04fd2ac4bc6cf278f15392774ac4
MakenaG <makenaglory44@gmail.com>
2023-03-05 22:43:44 +0300

    user.rb updated

--- db/migrate/20230304193323_create_memes.rb
+++ db/migrate/20230304193323_create_memes.rb
@@ -2,14 +2,10 @@ class CreateMemes < ActiveRecord::Migration[6.1]
   def change
     #create columns
     create_table :memes do |t|
-      t.references :user, foreign_key: { on_delete: :cascade }
-      t.string :name, :null => false
-      t.string :top_caption, :null => false
-      t.string :bottom_caption, :null => false
-      t.string :image, null: false
-      t.string :original_image, null: false
-      t.timestamps null: false
-
+      t.string  :title
+      t.string :message
+      t.integer :user_id
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_04_193323) do
+ActiveRecord::Schema.define(version: 2023_03_05_193158) do
 
   create_table "memes", force: :cascade do |t|
     t.integer "user_id"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2023_03_04_193323) do
     t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "memes", "users", on_delete: :cascade
